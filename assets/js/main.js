cacf8797103811ea162660fdd4d11313db8debd2
Pedro Perez <pedrowperez@gmail.com>
2021-07-26 10:41:28 -0300

    fix: home desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,22 @@ console.log(mobile);
 
 const firstScene = $("#first-vitrine").get(0);
 const secondScene = $("#second-vitrine").get(0);
+const aboutScene = $('#first-section').get(0);
 
 const firstParallax = new Parallax(firstScene, {
   pointerEvents: true
 });
 
+const aboutParallax = new Parallax(aboutScene, {
+  pointerEvents: true,
+  invertX: false,
+  calibrateX: true,
+});
+
 const secondParallax = new Parallax(secondScene, {
-  pointerEvents: true
+  pointerEvents: true,
+  invertX: false,
+  invertY: false,
 });
 
 document.getElementById("btn-nav").addEventListener("click", changeONOf);
@@ -29,3 +38,37 @@ function changeONOf() {
     btn.classList.remove("on");
   }
 }
+
+
+
+// const bgAgronegocioVG = document.querySelector('#geral-agronegocio > #bg');
+// const estabilidadeAgronegocioVG = document.querySelector('#geral-agronegocio > #estabilidade');
+// const economiaAgronegocioVG = document.querySelector('#geral-agronegocio > #economia');
+
+// const yellowestabilidadeAgronegocioVG = document.querySelector('#geral-agronegocio > #yellowestabilidadeAgronegocioSVG');
+// const yelloweconomiaAgronegocioVG = document.querySelector('#geral-agronegocio > #yelloweconomiaAgronegocioSVG');
+
+// const tlestabilidadeAgronegocio = new TimelineLite();
+
+// tlestabilidadeAgronegocio
+//     .to(yellowestabilidadeAgronegocioVG, .35, {
+//         opacity: .8,
+//     })
+//     .to(estabilidadeAgronegocioVG, .35, {
+//         opacity: 1,
+//         yoyo: true
+//     }, "-=0.75")
+
+
+// tlestabilidadeAgronegocio.pause();
+
+// $(estabilidadeAgronegocioVG).hover(function () {
+//     tlestabilidadeAgronegocio.play();
+//     $('#infoAgronegocio h2').html('Economia');
+//     $('#infoAgronegocio p').html('');
+// }, function () {
+//     tlestabilidadeAgronegocio.reverse();
+ 
+// }
+// );
+
